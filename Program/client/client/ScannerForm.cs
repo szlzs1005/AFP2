d01fa2d76fa9b8851547c138954e002aed199f7a
Mihály Balázs <75239032+Bazsi201@users.noreply.github.com>
2023-05-20 10:00:10 +0200

    Build:ScannerForm

---
+++ Program/client/client/ScannerForm.cs
@@ -0,0 +1,30 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace client
+{
+    public partial class ScannerForm : Form
+    {
+        RestClient client = null;
+        RestRequest request = null;
+        RestResponse<List<Hash>> response = null;
+        string fileName = null;
+        public ScannerForm()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
