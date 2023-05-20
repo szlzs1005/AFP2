935d59d17066fedf5c631a706597ff76efef1803
Mihály Balázs <75239032+Bazsi201@users.noreply.github.com>
2023-05-20 10:01:10 +0200

    func:btnScan_Click

--- Program/client/client/ScannerForm.cs
+++ Program/client/client/ScannerForm.cs
@@ -36,6 +36,64 @@ namespace client
             }
         }
 
-    
+    private void btnScan_Click(object sender, EventArgs e)
+        {
+            if (tbFilePath.Text == "" || tbMD5.Text == "")
+            {
+                lblStatus.ForeColor = Color.Red;
+                lblStatus.Text = "Please select a file";
+                return;
+            }
+            try
+            {
+                client = new RestClient("http://localhost:3001/hash/read");
+                request = new RestRequest();
+                request.Method = Method.Get;
+                response = client.Execute<List<Hash>>(request);
+                bool infected = false;
+                foreach (Hash hash in response.Data)
+                {
+                    if (hash.value == tbMD5.Text)
+                    {
+                        infected = true;
+                        break;
+                    }
+                }
+                if (infected)
+                {
+                    lblStatus.ForeColor = Color.Red;
+                    lblStatus.Text = "infected";
+                    if (MessageBox.Show("Do you want to move the file?", "Infected file", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                    {
+                        string path = @tbFilePath.Text;
+                        string path2 = @"C:\Users\zsolt\OneDrive\Asztali gép\Egyetem\4. félév\afp2\client\client\infectedFiles\" + fileName;
+                        try
+                        {
+                            File.Move(path, path2);
+                            MessageBox.Show(fileName + " moved successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            tbFilePath.Text = "";
+                            tbMD5.Text = "";
+                            lblStatus.ForeColor = Color.Black;
+                            lblStatus.Text = "Status:N/A";
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message, "Error",MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                }
+                else
+                {
+                    lblStatus.ForeColor = Color.Green;
+                    lblStatus.Text = "clean";
+                }
+            }
+            catch
+            {
+                lblStatus.ForeColor = Color.Red;
+                lblStatus.Text = "Something went wrong";
+            }
+            
+        }
     }
 }
