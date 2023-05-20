127abc09fea4ae3eccf7dfb57fbe7a2dbdc58041
Szabó László Zsolt <zsoltisw@gmail.com>
2023-05-20 10:10:27 +0200

    Build:AddForm

---
+++ Program/client/client/AddForm.cs
@@ -0,0 +1,31 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace client
+{
+    public partial class AddForm : Form
+    {
+        RestClient client = null;
+        RestRequest request = null;
+        RestResponse response = null;
+        Response res = null;
+        public AddForm()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
